45a60406e116efc07f8fb459fd45db4e5f4461b0
rajan <rajanshahi1267@gmail.com>
2023-06-28 23:23:32 -0700

    my code initial

---
+++ src/components/home/Homebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./homebar.css"
+
+export default function Homebar() {
+  return (
+    <div>
+    <div className="TopBar-Containner">Loki</div>
+
+
+
+    <div className="HomeBar-Containner">Rajan</div>
+
+
+
+    <div className="LastBar-Containner">Chaulagain &Shahi</div>
+    </div>
+  )
+}
